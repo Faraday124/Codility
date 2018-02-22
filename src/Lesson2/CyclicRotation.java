a76734132ecb16888aa27c871a73de26bd4cce83
Faraday124 <iwo.paprota@gmail.com>
2018-02-22 23:18:03 +0100

    New Algorithms

--- src/Lesson2/CyclicRotation.java
+++ src/Lesson2/CyclicRotation.java
@@ -20,8 +20,8 @@ public class CyclicRotation {
         int[] result = new int[original.length];
         System.arraycopy(original, 0, result, 0, original.length);
         for (int i = 0; i < original.length; i++) {
-            if (i != original.length-1) {
-                result[i+1] = original[i];
+            if (i != original.length - 1) {
+                result[i + 1] = original[i];
             } else {
                 result[0] = original[i];
             }
@@ -32,9 +32,51 @@ public class CyclicRotation {
 
     public static void main(String[] args) {
         CyclicRotation r = new CyclicRotation();
-        int[] x = new int[] { 7, 3, 4, 5 };
-        int [] result = r.solution(x, 1);
+        int[] x = new int[]{7, 3, 4, 5};
+        int[] result = r.solution(x, 1);
         System.out.println(Arrays.toString(result));
     }
-
 }
+
+/***
+ A zero-indexed array A consisting of N integers is given.
+ Rotation of the array means that each element is shifted right by one index,
+ and the last element of the array is moved to the first place.
+ For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]
+ (elements are shifted right by one index and 6 is moved to the first place).
+
+ The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+ Write a function:
+
+ class Solution { public int[] solution(int[] A, int K); }
+
+ that, given a zero-indexed array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+ For example, given
+
+ A = [3, 8, 9, 7, 6]
+ K = 3
+ the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+ [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+ [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+ [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+ For another example, given
+
+ A = [0, 0, 0]
+ K = 1
+ the function should return [0, 0, 0]
+
+ Given
+
+ A = [1, 2, 3, 4]
+ K = 4
+ the function should return [1, 2, 3, 4]
+
+ Assume that:
+
+ N and K are integers within the range [0..100];
+ each element of array A is an integer within the range [−1,000..1,000].
+ In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+ */
