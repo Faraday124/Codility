4643df200dcfc49c0ed0a5a712bf1df82a12577f
Iwo Paprota <iwo.paprota@ocado.com>
2020-03-29 21:45:00 +0200

    Added Kotlin dependency and a task accomplished in Kotlin: Triangle.kt

--- src/main/kotlin/lesson6/Triangle.kt
+++ src/main/kotlin/lesson6/Triangle.kt
@@ -8,7 +8,8 @@ fun solution(A: IntArray): Int {
     A.sort()
 
     for (i in 0 until A.size - 2) {
-        if (A[i] + A[i + 1] > A[i + 2]) {
+        val tempSum: Long  = A[i].toLong()  + A[i + 1].toLong()
+        if (tempSum > A[i + 2]) {
             return 1
         }
     }
